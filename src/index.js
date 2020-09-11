d939030692c060953e0d6d7bb17d5e5e6b7aa4d0
Rusel <somnus085@hotmail.com>
2020-09-11 10:00:17 -0500

    Esqueleto del proyecto con webpack instalado y configurado, incluyendo babel

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { saludar } from './js/componentes.js';  // ASI SE IMPORTA PARA WEB; PARA NODEJS SE USA REQUIRE
+import './style.css';
+
+//import img from './assets/webpack-logo.png'   // SI quisiera importar una imágen para usar desde el javascript
+
+const nombre='Rusel Arceo Monforte';
+saludar(nombre);
